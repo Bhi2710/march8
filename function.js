67bb6f4c47fbd2543056cdad93e2d84641978d2f
Bhi2710 <92436824+Bhi2710@users.noreply.github.com>
2022-03-13 18:09:56 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,47 @@
+function popup(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?fit=crop&format=jpg&crop=4560,2565,x790,y784,safe";  
+}
+function popup1(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d37033a95e0230008f64eb2/2020-Aston-Martin-Rapide-E/0x0.jpg?fit=crop&format=jpg&crop=3000,1687,x0,y157,safe"; 
+}
+function popup2(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d3703b3090f4300070d570d/2020-Cadillac-CT5/0x0.jpg?fit=crop&format=jpg&crop=4842,2723,x288,y538,safe";  
+}
+function popup3(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d37049295e0230008f64eeb/2020-Kia-Soul-EV/0x0.jpg?fit=crop&format=jpg&crop=4377,2463,x547,y592,safe";  
+}
+function popup4(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d3703e2f1176b00089761a6/2020-Chevrolet-Corvette-Stingray/0x0.jpg?fit=crop&format=jpg&crop=4560,2565,x836,y799,safe";  
+}
+function popup5(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d37046395e0230008f64edf/2020-Ford-Mustang-Shelby-GT500/0x0.jpg?fit=crop&format=jpg&crop=2552,1437,x299,y201,safe";  
+}
+function popup6(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d35eb15f1176b0008974b5c/2020-McLaren-GT/0x0.jpg?fit=crop&format=jpg&crop=4800,2701,x512,y602,safe";  
+}
+function popup7(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d3704d0f1176b00089761ae/2020-Mini-Cooper-SE/0x0.jpg?fit=crop&format=jpg&crop=4841,2723,x474,y297,safe";  
+}
+function popup8(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d3704fb090f4300070d573d/2020-Polestar-1/0x0.jpg?fit=crop&format=jpg&crop=2800,1576,x48,y361,safe";  
+}
+function popup9(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d37051e95e0230008f64f17/2020-Polestar-2/0x0.jpg?fit=crop&format=jpg&crop=2800,1575,x678,y1045,safe";  
+}
+function popup10(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d370543f1176b00089761ce/2020-Porsche-Taycan/0x0.jpg?fit=crop&format=jpg&crop=2641,1485,x329,y337,safe";  
+}
+function popup11(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d35eb4795e0230008f63724/2020-Tesla-Roadster/0x0.jpg?fit=crop&format=jpg&crop=3072,1728,x282,y157,safe";  
+}
+function popup12(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d37057895e0230008f64f45/2020-Toyota-GR-Supra/0x0.jpg?fit=crop&format=jpg&crop=2880,1620,x242,y229,safe";  
+}
+function popup13(){
+    var a = document.getElementById("change").src="https://imageio.forbes.com/specials-images/imageserve/5d3704d0f1176b00089761ae/2020-Mini-Cooper-SE/0x0.jpg?fit=crop&format=jpg&crop=4841,2723,x474,y297,safe";  
+}
+
+function removed(){
+    var a = document.getElementById("change")
+    a.classList.add("d-none");
+}
